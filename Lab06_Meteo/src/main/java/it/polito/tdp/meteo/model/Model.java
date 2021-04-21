1de6ed340f9275406c1a865117b7759a9483810f
Leo Volpe <Leo Volpe@LAPTOP-VU9KPGVR.station>
2021-04-21 11:40:46 +0200

    funziona ma non trova la soluzione migliore

--- Lab06_Meteo/src/main/java/it/polito/tdp/meteo/model/Model.java
+++ Lab06_Meteo/src/main/java/it/polito/tdp/meteo/model/Model.java
@@ -17,11 +17,13 @@ public class Model {
 	private List<Citta> soluzioneFinale;
 	private float costoSoluzMigliore;
 	private int giorniCons;
+	private boolean trovato;
 	
 	public Model() {
 		this.meteoDAO= new MeteoDAO();
 		this.costoSoluzMigliore=0;
-		//this.giorniCons=1;
+		this.giorniCons=1;
+		trovato=true;
 		this.soluzioneFinale= new ArrayList<>();
 	}
 
@@ -33,89 +35,106 @@ public class Model {
 	// of course you can change the String output with what you think works best
 	public String trovaSequenza(int mese) {
 		List<Citta> citta= this.meteoDAO.getAllCitta();
-		Map<Citta,List<Rilevamento>> rilevamenti= new HashMap<>();
-		Map<Citta,Integer> giorniCitta= new HashMap<>();
+		//Map<Citta,List<Rilevamento>> rilevamenti= new HashMap<>();
+		//Map<Citta,Integer> giorniCitta= new HashMap<>();
 		for (Citta c : citta) {
-			rilevamenti.put(c, new ArrayList<Rilevamento>(this.meteoDAO.getAllRilevamentiLocalitaMese(mese, c.getNome())));
-			giorniCitta.put(c, 0);
+			List<Rilevamento> rilev= new ArrayList<Rilevamento>(this.meteoDAO.getAllRilevamentiLocalitaMese(mese, c.getNome()));
+			c.setRilevamenti(rilev);
+			//rilevamenti.put(c,rilev);
+			//giorniCitta.put(c, 0);
 		}
-		int tot=0;
 		List<Citta> parziale = new ArrayList<Citta>();
-		ricorsione(0,citta,parziale,tot,giorniCitta,rilevamenti);
+		ricorsione(citta,parziale);
 		String ris= this.SoluzStringa(soluzioneFinale);
 		return ris;
 	}
 	
-	private void ricorsione(int livello, List<Citta> citta, List<Citta> parziale, int tot, Map<Citta,Integer> giorniCitta, Map<Citta,List<Rilevamento>> rilevamenti) {
-		if (livello==(this.NUMERO_GIORNI_TOTALI-1)) { //terminale
-			if (isValid(parziale, giorniCitta)) {
-				//CHECK SE E' PREZZO MIGLIORE
-				if (livello==0) { //prima esecuzione
-					this.costoSoluzMigliore=tot;
-				} else {
-					if (tot<this.costoSoluzMigliore) {
-						this.costoSoluzMigliore=tot;
-						this.soluzioneFinale.clear();
-						this.soluzioneFinale.addAll(parziale);
-					}
+	private void ricorsione(List<Citta> citta, List<Citta> parziale) {
+		if (parziale.size()==(this.NUMERO_GIORNI_TOTALI)) { //terminale
+			//CHECK SE E' PREZZO MIGLIORE
+			if (trovato) { //prima esecuzione
+				this.soluzioneFinale.addAll(parziale);
+				this.costoSoluzMigliore=this.costoSoluzione(soluzioneFinale);
+				trovato=false;
+			} else {
+				if (this.costoSoluzione(parziale)<this.costoSoluzMigliore) {
+					this.costoSoluzMigliore=this.costoSoluzione(parziale);
+					this.soluzioneFinale.clear();
+					this.soluzioneFinale.addAll(parziale);
 				}
 			}
 		} 
 		else {
 			for (Citta c : citta) {
-				parziale.add(c);
-				giorniCitta.put(c, giorniCitta.get(c)+1); //oppure aggiono il counter
-				tot+= rilevamenti.get(c).get(livello).getUmidita();
-				if (livello>1) {
-					if (!parziale.get(livello-1).equals(parziale.get(livello-2)))
-						tot+=this.COST;
-				}
-				ricorsione(livello+1,citta,parziale,tot,giorniCitta,rilevamenti);
-				parziale.remove(citta);
-				
-				//BACKTRACKING
-				if (livello!=0 ) {
-					if (livello>1) {
-						if (!parziale.get(livello-1).equals(parziale.get(livello-2)))
-							tot-=this.COST;
+				if (isValid(parziale,c,citta)) { //+giorniCitta
+					parziale.add(c);
+					c.increaseCounter();
+					ricorsione(citta,parziale);
+					
+					//BACKTRACKING
+					parziale.remove(parziale.size()-1);
+					c.decreaseCounter();
 					}
-					tot-= rilevamenti.get(c).get(livello).getUmidita();
-					parziale.remove(livello-1);
-					giorniCitta.put(c, giorniCitta.get(c)-1);
+				}
+		}
+	}
+	
+	private boolean isValid(List<Citta>parziale, Citta city, List<Citta> citta) {
+		city.increaseCounter();
+		//NESSUNA CITTA CON PIU DI 6 GIORNI
+		for (Citta c : citta) {
+			if (c.getCounter()>NUMERO_GIORNI_CITTA_MAX) {
+				c.decreaseCounter();
+				return false;
+			}	
+		}
+		
+		//TUTTE LE CITTA PRESENTI NELLA SOLUZIONE
+		if (parziale.size()>=12) {
+			for (Citta c: citta) {
+				if (c.getCounter()==0) {
+					city.decreaseCounter();
+					return false;
 				}
 			}
 		}
 		
-	}
-	
-	private boolean isValid(List<Citta>parziale, Map<Citta,Integer> giorniCitta) {
-		//NESSUNA CITTA CON PIU DI 6 GIORNI
-		for (Citta c : giorniCitta.keySet()) {
-			if (giorniCitta.get(c)>this.NUMERO_GIORNI_CITTA_MAX)
-				return false;
-		}
-		
-		//TUTTE LE CITTA PRESENTI NELLA SOLUZIONE
-		for (Citta c: giorniCitta.keySet()) {
-			if (giorniCitta.get(c)==0)
-				return false;
-		}
-		
 		//CHECK SUI GIORNI MINIMI CONSECUTIVI
-		for (int i=1; i<this.NUMERO_GIORNI_TOTALI; i++) {
+		if (!parziale.isEmpty())
+			if (city.equals(parziale.get(parziale.size()-1))) 
+				giorniCons++;
+			else {
+				if (parziale.size()>2) {
+					if(!parziale.get(parziale.size()-1).equals(parziale.get(parziale.size()-2))) 
+						return false;
+					giorniCons=1;
+				} else
+					return false;
+			}
+		/*for (int i=0; i<parziale.size(); i++) {
 			if (i!=0) {
 				if (parziale.get(i).equals(parziale.get(i-1)))
 					this.giorniCons++;
 				else {
-					if (giorniCons<this.NUMERO_GIORNI_CITTA_CONSECUTIVI_MIN)
+					if (this.giorniCons<NUMERO_GIORNI_CITTA_CONSECUTIVI_MIN)
 						return false;
 					this.giorniCons=1;
 				}
-			} 
-			else
-				this.giorniCons=1;
-		}
+			}
+		}*/
+		
+		city.decreaseCounter();
 		return true;
+	}
+	
+	public float costoSoluzione (List<Citta> soluzione) {
+		float c =0;
+		for (int i=0; i<soluzione.size(); i++) {
+			if (i!=0 && soluzione.get(i).equals(soluzione.get(i-1)))
+				c+= COST;
+			c = c + soluzione.get(i).getRilevamenti().get(i).getUmidita();
+		}
+		return c;
 	}
 
 	public String SoluzStringa(List<Citta> soluzione) {
